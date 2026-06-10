2c7cce8f6081df23f02e0ac2afe9aac42b98fbf6
MilitsaTsvetkova <militsa88@yahoo.com>
2026-06-10 15:25:45 +0300

    connect fe and be

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error("Error fetching message:", error));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>{message || "Loading..."}</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ packages/server/index.ts
@@ -0,0 +1,15 @@
+import express, { type Request, type Response } from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!!!!");
+});
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.json({ message: "Hello from the API!" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
