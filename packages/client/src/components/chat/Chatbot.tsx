0fa67c65ca6501fba062b83aab6b4b29fcfa2f43
MilitsaTsvetkova <militsa88@yahoo.com>
2026-06-15 17:06:54 +0300

    add sound

--- packages/client/src/components/chat/Chatbot.tsx
+++ packages/client/src/components/chat/Chatbot.tsx
@@ -1,10 +1,17 @@
 import axios from 'axios';
 import { useRef, useState } from 'react';
+import notificationSound from '../../assets/sounds/notification.mp3';
+import popSound from '../../assets/sounds/pop.mp3';
 import ChatInput, { type ChatFormData } from './ChatInput';
 import type { Message } from './ChatMessages';
 import ChatMessages from './ChatMessages';
 import TypingIndicator from './TypingIndicator';
 
+const popAudio = new Audio(popSound);
+popAudio.volume = 0.2;
+
+const notificationAudio = new Audio(notificationSound);
+notificationAudio.volume = 0.2;
 type ChatResponse = {
   message: string;
 };
@@ -19,6 +26,7 @@ const Chatbot = () => {
     setMessages((prev) => [...prev, { role: 'user', content: prompt }]);
     setIsBotTyping(true);
     setError(null);
+    popAudio.play();
 
     try {
       const { data } = await axios.post<ChatResponse>('/api/chat', {
@@ -27,6 +35,7 @@ const Chatbot = () => {
       });
 
       setMessages((prev) => [...prev, { role: 'bot', content: data.message }]);
+      notificationAudio.play();
     } catch (error) {
       console.error('Error fetching response:', error);
       setError('An error occurred while fetching the response.');
